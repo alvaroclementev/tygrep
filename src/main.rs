5bc4b57f48c204538942fa7f7501c92efb514ddb
Alvaro Clemente <clemente.varo@gmail.com>
2023-04-23 20:31:22 +0200

    feat: SCIP index loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use protobuf::Message;
+use scip::types::Index;
+use std::{path::Path, time::Instant};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new("samples/rust-regex.scip");
+
+    let start = Instant::now();
+    let contents = std::fs::read(path)?;
+    println!("Read {} bytes of contents", contents.len());
+
+    // Deserializing
+    let mut index = Index::default();
+    index.merge_from_bytes(&contents)?;
+
+    let end = Instant::now();
+    // println!("We have an index!, {:?}", index);
+    println!(
+        "Elapsed time loading SCIP index: {:.2}ms",
+        (end - start).as_millis()
+    );
+
+    Ok(())
 }
